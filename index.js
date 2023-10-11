a755fa833dcd70500480fa4765eca6a4f18078f7
AylenRondan <rrondancarrizo@gmail.com>
2023-10-11 09:32:17 -0300

    poc/ServicioControlador

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log ("Hola mundo (?") 
+ /* 
+Controlador:
+Es una clase que se encarga de recibir la peticion del cliente. Aqui se indica si son metodos GET, PUT, POST, ETC. Tambien se define aqui los objetos de entrada y salida (los DTOs que se va a utilizar) o si esq se necesita queryParams, uriParams.
+
+Servicio:
+Es una clase que se encarga de la parte logica, por ejemplo aqui se realizan las llamadas a respositorios y se trabajarn sobre los datos de las entidades, mapeos, la logica( switch, if, o cualquier tipo de logica necesaria)
+
+En palabras para que se entienda el controlador vendria a ser como el gerente, mientras que el Servicio es el trabajador. 
+*/
+
